e1449ff794e948fb50918fe3cd94d03f6f0d38f6
A1909bek <aa2004bek@gmail.com>
2025-02-06 15:16:08 +0000

    next commit

---
+++ accounts/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.contrib.auth.forms import UserCreationForm,UserChangeForm
+from .models import CustomUser
+# Register your models here.
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = CustomUser
+        fields = ['username','email','age']
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta:
+        model = CustomUser
+        fields = ['username','email','age']
